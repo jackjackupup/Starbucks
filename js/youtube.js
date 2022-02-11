f5430ca39bf8e80b59ff4bbaa31e17099475b95b
jackjackupup <apatheia15@gmail.com>
2022-02-11 14:34:00 +0900

    first commit

---
+++ js/youtube.js
@@ -0,0 +1,27 @@
+    var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+    
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          videoId: 'An6LvWQuj_8',
+          playerVars: {
+              autoplay: true,
+              loop: true,
+              playerlist: 'An6LvWQuj_8'
+          },
+          events: {
+              onReady: function (event) {
+                event.target.mute()
+              }
+          }
+        });
+      }
+
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
